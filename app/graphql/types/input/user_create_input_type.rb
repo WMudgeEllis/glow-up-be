3305d2fd39870a7d5389ca76cfa5ec49e7dba850
Laura Court <laura.mcourt@gmail.com>
2021-12-01 00:56:26 -0500

    Add user types, queries, input type

---
+++ app/graphql/types/input/user_create_input_type.rb
@@ -0,0 +1,9 @@
+module Types
+  module Input
+    class UserCreateInputType < Types::BaseInputObject
+      argument :username, String, required: true
+      argument :password, String, required: true
+      argument :password_confirmation, String, required: true
+    end
+  end
+end
