bf947d11fd97f0104cb6f51c729e2e744cd5abbc
Laura Court <laura.mcourt@gmail.com>
2021-12-02 01:24:07 -0500

    Remove updated_at field

--- app/graphql/types/journal_entry_type.rb
+++ app/graphql/types/journal_entry_type.rb
@@ -6,6 +6,5 @@ module Types
     field :user_id, Integer, null: true
     field :journal_id, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
