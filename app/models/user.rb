c7ec85f7e060673c506aba02e9b27c7e1c50f8c0
Laura Court <laura.mcourt@gmail.com>
2021-12-02 13:34:13 -0500

    Merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :moods
+  has_many :journal_entries
+  has_many :journals, through: :journal_entries
+  has_many :habit_entries
+  has_many :habits, through: :habit_entries
 
   validates :username, presence: true, uniqueness: true
 end
