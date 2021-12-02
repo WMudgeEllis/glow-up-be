53d8c218a3a03864111a9fd703096d2c2e1292ec
Tanner Dale <tanner@tannerdale.com>
2021-12-01 22:13:44 -0600

    Add mood table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_041810) do
+ActiveRecord::Schema.define(version: 2021_12_02_035340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "moods", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "mood"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_moods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_12_01_041810) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "moods", "users"
 end
