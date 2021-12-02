bf50b157fe522dc4832dba5fda0eb4b46653cda5
Laura Court <laura.mcourt@gmail.com>
2021-12-02 13:34:48 -0500

    Merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+User.destroy_all
+Journal.destroy_all
+JournalEntry.destroy_all
+
 user = User.create!(
   username: 'User',
   password: 'hello',
@@ -11,3 +15,12 @@ user = User.create!(
     user_id: user.id
   )
 end
+
+j = Journal.create!(name: 'daily', details: 'J')
+
+j.journal_entries.create!(date: Date.today, content: 'JJJJJJJJ', user_id: User.first.id)
+j.journal_entries.create!(date: Date.today, content: 'AAAAAAAA', user_id: User.last.id)
+
+h = Habit.create!(name: 'h1', description: 'd1')
+
+h.habit_entries.create!(date: Date.today, status: 1, user_id: User.first.id, habit_id: h.id)
