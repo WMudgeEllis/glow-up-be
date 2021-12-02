1532e8f1f557a474c2a97dcc0b232a8751e61c52
Laura Court <laura.mcourt@gmail.com>
2021-12-02 01:24:51 -0500

    Add journal entries by user method

--- app/graphql/types/journal_type.rb
+++ app/graphql/types/journal_type.rb
@@ -4,6 +4,12 @@ module Types
     field :name, String, null: true
     field :details, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :journal_entries, [Types::JournalEntryType], null: true do
+      argument :user_id, Integer
+    end
+
+    def journal_entries(user_id:)
+      object.journal_entries.where(user_id: user_id)
+    end
   end
 end
