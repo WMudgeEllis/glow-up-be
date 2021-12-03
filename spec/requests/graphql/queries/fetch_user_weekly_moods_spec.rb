b618b4a1f2db274c5f6529f94ce3f36aca26824b
Tanner Dale <tanner@tannerdale.com>
2021-12-02 20:34:50 -0600

    rename

---
+++ spec/requests/graphql/queries/fetch_user_weekly_moods_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'Fetch User Weekly Moods' do
+  let!(:user) { create :user }
+
+  let(:json) { JSON.parse(response.body, symbolize_names: true) }
+  let(:reduced) { json[:data] }
+  let(:data) { reduced[:fetchUser] }
+
+  before :each do
+    create_list :mood, 7, user_id: user.id
+    sleep(1)
+    create_list :mood, 3, user_id: user.id
+
+    post '/graphql', params: { query: query }
+  end
+
+  it 'returns past seven moods' do
+    result = data[:weeklyMoods].first[:createdAt] > data[:weeklyMoods].last[:createdAt]
+
+    expect(json).to_not have_key(:errors)
+    expect(data[:weeklyMoods].length).to eq(7)
+    expect(result).to be(true)
+  end
+
+  def query
+    <<~GQL
+      query fetchUser {
+        fetchUser {
+          id
+          weeklyMoods {
+            createdAt
+            description
+            id
+            mood
+          }
+        }
+      }
+    GQL
+  end
+end
