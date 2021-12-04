8a6b494af1ed7087bde7c438db257330ab5d2f7e
Tanner Dale <tanner@tannerdale.com>
2021-12-04 17:03:33 -0600

    remove created at field

--- app/graphql/types/habit_type.rb
+++ app/graphql/types/habit_type.rb
@@ -3,7 +3,6 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :description, String, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :habit_entries, [Types::HabitEntryType]
   end
 end
