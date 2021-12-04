d277421be48e12686e9aa3cb77fbb86041ee2c5f
Weston Ellis <ellisweston112@gmail.com>
2021-12-03 18:01:10 -0700

    Remove query

--- app/graphql/types/habit_type.rb
+++ app/graphql/types/habit_type.rb
@@ -5,15 +5,5 @@ module Types
     field :description, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :habit_entries, [Types::HabitEntryType]
-    field :weekly_habit_entries, [Types::HabitEntryType]
-
-    def weekly_habit_entries
-      #if we add multiple users this needs to be changed, only adding it to maybe make the future easier
-      user = User.first
-      object.habit_entries
-            .where('user_id = ?', user.id)
-            .order(created_at: :desc)
-            .limit(7)
-    end
   end
 end
