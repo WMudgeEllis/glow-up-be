e36e4683507c0c49ee75eeedba7b28671f56fea4
Weston Ellis <ellisweston112@gmail.com>
2021-12-04 15:07:34 -0700

    Write model test

--- spec/requests/graphql/mutations/create_habit_entry_spec.rb
+++ spec/requests/graphql/mutations/create_habit_entry_spec.rb
@@ -1,20 +1,28 @@
 require "rails_helper"
 
-RSpec.describe 'create habit entry' do
+describe 'create habit entry' do
 
   before :each do
     @user = create(:user)
-    create(:habit, id: 1)
-    create(:habit, id: 2)
+    @habit = create(:habit)
+    create(:habit, id: 7)
+    create(:habit, id: 13)
   end
 
   it 'can create a habit entry' do
     post '/graphql', params: { query: query }
 
-    parsed = JSON.parse(response.body, symbolyze_names: true)
-    
-    expect(@user.habit_entries.count).to eq(2)
+    expect(@user.habit_entries.count).to eq(3)
     expect(@user.habit_entries.first.status).to eq(1)
+    expect(@habit.habit_entries.first.status).to eq(0)
+  end
+
+  it 'returns errors when given bad query' do
+    post '/graphql', params: { query: bad_query }
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:errors)
   end
 
   def query
@@ -22,7 +30,23 @@ RSpec.describe 'create habit entry' do
     mutation {
       createHabitEntry(
         input: {
-          params: [{id: 1}, {id: 2}]
+          params: [{id: 13}, {id: 7}]
+        }
+      ) {
+        user {
+          id
+        }
+      }
+    }
+    GQL
+  end
+
+  def bad_query
+    <<~GQL
+    mutation {
+      createHabitEntry(
+        input: {
+          haha_evil_query
         }
       ) {
         user {
