726e70f4826da61ded2d2faeeafcec2e3ae23760
Weston Ellis <ellisweston112@gmail.com>
2021-12-03 18:03:01 -0700

    Update factory

--- spec/factories/habit_entries.rb
+++ spec/factories/habit_entries.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :habit_entry do
-    status { 1 }
-    user { nil }
-    habit { nil }
+    date { Date.today }
+    status { [0, 1].sample }
+    association :user
+    association :habit
   end
 end
