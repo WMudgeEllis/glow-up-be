dde9ce7c1291bbaae9319cc6b7c2d89b0bb0f290
Weston Ellis <ellisweston112@gmail.com>
2021-12-04 15:00:44 -0700

    Write model test

--- spec/models/habit_entry_spec.rb
+++ spec/models/habit_entry_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe HabitEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = create(:user)
+    @habit = create(:habit)
+    create_list(:habit, 14)
+    create(:habit_entry, user_id: @user.id, habit_id: @habit.id, status: 1)
+  end
+
+  it 'creates habit entries for neglected habits' do
+    HabitEntry.create_neglected(@user, [{id: @habit.id}])
+
+    expect(@user.habit_entries.count).to eq(15)
+    expect(HabitEntry.last.status).to eq(0)
+  end
 end
