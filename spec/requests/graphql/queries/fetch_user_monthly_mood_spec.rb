5d4beb9a3eab99b39f92bafb42114e37139d7700
Laura Court <laura.mcourt@gmail.com>
2021-12-03 20:39:04 -0500

    Update test for stubbed date

--- spec/requests/graphql/queries/fetch_user_monthly_mood_spec.rb
+++ spec/requests/graphql/queries/fetch_user_monthly_mood_spec.rb
@@ -4,23 +4,23 @@ describe 'Fetch User Query Monthly Moods' do
   let(:json) { JSON.parse(response.body, symbolize_names: true) }
   let(:reduced) { json[:data] }
   let(:data) { reduced[:fetchUser] }
-
   let!(:user) { create :user }
-  let!(:moods) do
+  before :each do
+    allow(Date).to receive(:today).and_return Date.new(2021,3,28)
     (0..40).to_a.each do |num|
       mood = create :mood, user_id: user.id
       mood.update_attribute(:created_at, Date.today - num)
     end
+
+    post '/graphql', params: { query: query }
   end
 
-  before { post '/graphql', params: { query: query } }
 
   it 'returns moods for current month' do
-    # require "pry"; binding.pry
     result = data[:monthlyMoods].first[:createdAt] > data[:monthlyMoods].last[:createdAt]
 
     expect(json).to_not have_key(:errors)
-    expect(data[:monthlyMoods].length).to eq(2)
+    expect(data[:monthlyMoods].length).to eq(28)
     expect(result).to be(true)
   end
 
