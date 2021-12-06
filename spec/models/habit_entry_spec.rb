5a14cd699c600f416af9401d97c67c9a1f564e17
Weston Ellis <ellisweston112@gmail.com>
2021-12-06 16:28:37 -0700

    Rebase

--- spec/models/habit_entry_spec.rb
+++ spec/models/habit_entry_spec.rb
@@ -20,6 +20,39 @@ RSpec.describe HabitEntry, type: :model do
       expect(@user.habit_entries.count).to eq(15)
       expect(HabitEntry.last.status).to eq(0)
     end
+    
+    it 'does not duplicate neglected habits' do
+      create(:habit_entry, user_id: @user.id, habit_id: @habit.id, status: 0)
+
+      HabitEntry.create_neglected(@user, [])
+
+      expect(@user.habit_entries.count).to eq(15)
+    end
+
+    it 'creates habit entries for completed habits' do
+      HabitEntry.create_entries(@user, [{id: @habit.id}, {id: 2}])
+
+      expect(@user.habit_entries.count).to eq(15)
+      expect(@habit.habit_entries.first.status).to eq(1)
+      expect(HabitEntry.last.status).to eq(0)
+    end
+
+    it 'can update habit entries' do
+      HabitEntry.create_neglected(@user, [])
+
+      HabitEntry.create_entries(@user, [{id: @habit.id}])
+
+      expect(@user.habit_entries.count).to eq(15)
+      expect(@habit.habit_entries.first.status).to eq(1)
+    end
+
+    it 'is date sensitive' do
+      create(:habit_entry, user_id: @user.id, habit_id: @habit.id, created_at: Date.today - 1)
+
+      HabitEntry.create_entries(@user, [{id: @habit.id}])
+
+      expect(@user.habit_entries.count).to eq(16)
+    end
   end
 
   describe 'daily completed' do
@@ -38,4 +71,5 @@ RSpec.describe HabitEntry, type: :model do
       expect(user.habit_entries.daily_completed).to eq(completed_today.map(&:habit_id))
     end
   end
+
 end
