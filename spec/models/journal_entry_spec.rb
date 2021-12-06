8028b8d80fbcce42ee3b0ba8952468b7386e8378
Laura Court <laura.mcourt@gmail.com>
2021-12-05 23:58:18 -0500

    Rebase

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe JournalEntry, type: :model do
     it { should belong_to :user }
     it { should belong_to :journal }
   end
+  describe 'scopes' do
+
+    let!(:user) { create :user }
+    let!(:journal_entry) { create_list :journal_entry, 7, user_id: user.id }
+    let!(:past_journal_entries) { create_list :journal_entry, 5, created_at: Date.today - 8, user_id: user.id }
+
+    it 'returns last 7 journal entries' do
+      expect(user.journal_entries.weekly_journal).to eq(journal_entry.reverse)
+    end
+  end
 end
