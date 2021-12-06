cf5ba479a9a505937627f75602e3864935531ed5
Weston Ellis <ellisweston112@gmail.com>
2021-12-06 16:33:21 -0700

    Rebase

--- app/models/habit_entry.rb
+++ app/models/habit_entry.rb
@@ -2,28 +2,28 @@ class HabitEntry < ApplicationRecord
   belongs_to :user
   belongs_to :habit
 
-  def self.create_neglected(user, habit_ids)
-    neglected = Habit.where.not(id: habit_ids)
-    neglected.each do |habit|
-      user.habit_entries.create!(habit_id: habit.id, status: 0)
-    end
-  end
-
   def self.create_entries(user, habits)
     destroy_today_entries(user)
-    habit_ids = habits.map { |habits| habits[:id] }
+    completed_ids = habits.map { |habits| habits[:id] }
 
-    habit_ids.each do |habit_ids|
-      user.habit_entries.create!(habit_id: habit_ids, status: 1)
+    completed_ids.each do |habit_id|
+      user.habit_entries.create!(habit_id: habit_id, status: 1)
     end
 
-    create_neglected(user, habit_ids)
+    create_neglected(user, completed_ids)
   end
 
   def self.destroy_today_entries(user)
     where('created_at > ?', Date.today)
       .where(user_id: user.id)
       .destroy_all
+  end
+
+  def self.create_neglected(user, completed_ids)
+    neglected = Habit.where.not(id: completed_ids)
+    neglected.each do |habit|
+      user.habit_entries.create!(habit_id: habit.id, status: 0)
+    end
   end
   scope :completed, -> {
     where(status: 1)
