da6de1ea56eaf2647da17dd33738bf043dd18781
Weston Ellis <ellisweston112@gmail.com>
2021-12-08 14:14:11 -0700

    Refactor

--- app/models/habit_entry.rb
+++ app/models/habit_entry.rb
@@ -2,31 +2,6 @@ class HabitEntry < ApplicationRecord
   belongs_to :user
   belongs_to :habit
 
-  def self.create_entries(user, habits)
-    destroy_today_entries(user)
-    completed_ids = habits.map { |habits| habits[:id] }
-
-    completed_ids.each do |habit_id|
-      user.habit_entries.create!(habit_id: habit_id, status: 1)
-    end
-
-    create_neglected(user, completed_ids)
-  end
-
-  def self.destroy_today_entries(user)
-    # require "pry"; binding.pry
-    current_day
-      .where(user_id: user.id)
-      .destroy_all
-  end
-
-  def self.create_neglected(user, completed_ids)
-    neglected = Habit.where.not(id: completed_ids)
-    neglected.each do |habit|
-      user.habit_entries.create!(habit_id: habit.id, status: 0)
-    end
-  end
-
   scope :completed, -> {
     where(status: 1)
   }
@@ -40,4 +15,28 @@ class HabitEntry < ApplicationRecord
       .current_day
       .pluck(:habit_id)
   }
+
+  def self.destroy_today_entries(user)
+    current_day
+      .where(user_id: user.id)
+      .destroy_all
+  end
+
+  def self.create_neglected(user, completed_ids)
+    neglected = Habit.where.not(id: completed_ids)
+    neglected.each do |habit|
+      user.habit_entries.create!(habit_id: habit.id, status: 0)
+    end
+  end
+
+  def self.create_entries(user, habits)
+    destroy_today_entries(user)
+    completed_ids = habits.map { |habits| habits[:id] }
+
+    completed_ids.each do |habit_id|
+      user.habit_entries.create!(habit_id: habit_id, status: 1)
+    end
+
+    create_neglected(user, completed_ids)
+  end
 end
