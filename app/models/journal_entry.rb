e7dea4e254fb97693a16238732b1540253fa4833
Laura Court <laura.mcourt@gmail.com>
2021-12-09 17:02:33 -0500

    Create new journal entry method

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -5,4 +5,10 @@ class JournalEntry < ApplicationRecord
   scope :weekly_journal, -> {
     order(created_at: :desc).limit(7)
   }
+
+  scope :create_entries, ->(user, entries) {
+    entries.each do |entry_id|
+      user.journal_entries.create!(journal_entry_id: entries[:id])
+    end
+  }
 end
--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -8,11 +8,17 @@ RSpec.describe JournalEntry, type: :model do
   describe 'scopes' do
 
     let!(:user) { create :user }
-    let!(:journal_entry) { create_list :journal_entry, 7, user_id: user.id }
-    let!(:past_journal_entries) { create_list :journal_entry, 5, created_at: Date.today - 8, user_id: user.id }
+    let!(:journal) { create :journal}
+    let!(:journal_entry) { create_list :journal_entry, 7, user_id: user.id, journal_id: journal.id }
+    let!(:past_journal_entries) { create_list :journal_entry, 5, created_at: Date.today - 8, user_id: user.id, journal_id: journal.id }
 
     it 'returns last 7 journal entries' do
       expect(user.journal_entries.weekly_journal).to eq(journal_entry.reverse)
+    end
+
+    it 'creates new journal entries' do
+      JournalEntry.create!(content: 'something', user_id: user.id, journal_id: journal.id)
+      expect(journal.journal_entries.count).to eq(13)
     end
   end
 end
