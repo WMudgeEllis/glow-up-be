5919e18135859245cddaae2be6b9b40cf47b1abc
Laura Court <laura.mcourt@gmail.com>
2021-12-09 17:37:33 -0500

    Remove test for unused fields and methods

--- spec/requests/graphql/queries/fetch_user_relationships_spec.rb
+++ spec/requests/graphql/queries/fetch_user_relationships_spec.rb
@@ -22,20 +22,7 @@ describe 'Fetch User Query Relationships' do
   it 'returns user info' do
     expect(data[:id]).to_not be_empty
     expect(data[:username]).to_not be_empty
-    expect(data[:journals]).to_not be_empty
     expect(data[:journalEntries]).to_not be_empty
-    expect(data[:habits]).to_not be_empty
-    expect(data[:habitEntries]).to_not be_empty
-  end
-
-  it 'returns journal info' do
-    expect(data[:journals].size).to eq 1
-
-    data[:journals].each do |journal|
-      expect(journal[:id]).to_not be_empty
-      expect(journal[:name]).to_not be_empty
-      expect(journal[:details]).to_not be_empty
-    end
   end
 
   it 'returns journal entry info' do
@@ -45,25 +32,6 @@ describe 'Fetch User Query Relationships' do
       expect(entry[:id]).to_not be_empty
       expect(entry[:createdAt]).to_not be_empty
       expect(entry[:content]).to_not be_empty
-    end
-  end
-
-  it 'returns habit info' do
-    expect(data[:habits].size).to eq 1
-
-    data[:habits].each do |habit|
-      expect(habit[:id]).to_not be_empty
-      expect(habit[:name]).to_not be_empty
-    end
-  end
-
-  it 'returns habit entry info' do
-    expect(data[:habitEntries].size).to eq 5
-
-    data[:habitEntries].each do |entry|
-      expect(entry[:id]).to_not be_empty
-      expect(entry[:status]).to be_an Integer
-      expect(entry[:date]).to be_a String
     end
   end
 
@@ -81,24 +49,10 @@ describe 'Fetch User Query Relationships' do
         fetchUser {
           id
           username
-          journals {
-            id
-            name
-            details
-          }
           journalEntries {
             id
             createdAt
             content
-          }
-          habits {
-            id
-            name
-          }
-          habitEntries {
-            id
-            status
-            date
           }
           weeklyHabits {
             id
