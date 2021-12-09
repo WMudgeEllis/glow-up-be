a4ea6c6afbf5ee6815442d0a9722589d3b0b63f4
Laura Court <laura.mcourt@gmail.com>
2021-12-09 17:43:01 -0500

    Remove journal entry limit/rename method to all_journals

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -2,7 +2,7 @@ class JournalEntry < ApplicationRecord
   belongs_to :user
   belongs_to :journal
 
-  scope :weekly_journal, -> {
-    order(created_at: :desc).limit(7)
+  scope :all_journals, -> {
+    order(created_at: :desc)
   }
 end
