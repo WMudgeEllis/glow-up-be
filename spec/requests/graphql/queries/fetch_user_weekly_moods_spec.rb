887fb029805e19ee1fe72374c5d8b50b45fa29dc
Tanner Dale <tanner@tannerdale.com>
2021-12-13 11:20:36 -0600

    Udpate tests to only include current week if days missing

--- spec/requests/graphql/queries/fetch_user_weekly_moods_spec.rb
+++ spec/requests/graphql/queries/fetch_user_weekly_moods_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 describe 'Fetch User Weekly Moods' do
   let!(:user) { create :user }
   let!(:moods) do
-    create_list :mood, 7, user_id: user.id
-    sleep(1)
-    create_list :mood, 3, user_id: user.id
+    create_list :mood, 3, user_id: user.id, created_at: Date.today - 8
+
+    (0..7).each do |num|
+      create :mood, user_id: user.id, created_at: Date.today - num unless num == 2
+    end
   end
 
   let(:json) { JSON.parse(response.body, symbolize_names: true) }
@@ -18,7 +20,7 @@ describe 'Fetch User Weekly Moods' do
     result = data[:weeklyMoods].first[:createdAt] > data[:weeklyMoods].last[:createdAt]
 
     expect(json).to_not have_key(:errors)
-    expect(data[:weeklyMoods].length).to eq(7)
+    expect(data[:weeklyMoods].length).to eq(6)
     expect(result).to be(true)
   end
 
