aa00a501f07cfdc3893c0784266b98f4ec7d5e62
Laura Court <laura.mcourt@gmail.com>
2021-12-13 19:30:39 -0500

    Add date method

--- app/graphql/types/journal_entry_type.rb
+++ app/graphql/types/journal_entry_type.rb
@@ -1,10 +1,13 @@
 module Types
   class JournalEntryType < Types::BaseObject
     field :id, ID, null: false
-    field :date, GraphQL::Types::ISO8601DateTime, null: true
     field :content, String, null: true
     field :user_id, Integer, null: true
     field :journal_id, Integer, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :date, GraphQL::Types::ISO8601DateTime, null: true
+
+    def date
+      object.created_at
+    end
   end
 end
