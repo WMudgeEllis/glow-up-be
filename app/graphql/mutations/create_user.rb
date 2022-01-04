aeaf3caaf153c6220939071aac778da0012a3255
Laura Court <laura.mcourt@gmail.com>
2022-01-04 18:36:41 -0500

    Add token to user creation mutation
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,13 +2,16 @@ module Mutations
   class CreateUser < Mutations::BaseMutation
     argument :params, Types::Input::UserCreateInputType, required: true
 
+    field :token, String, null: true
     field :user, Types::UserType, null: false
 
     def resolve(params:)
       user_params = Hash params
       begin
         user = User.create!(user_params)
-        { user: user }
+        token = user.generate_token
+
+        { user: user, token: token }
       end
     end
   end
