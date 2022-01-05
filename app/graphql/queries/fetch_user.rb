cd1e486e0ec84e77e50db9940db84e2f33e97e3a
Laura Court <laura.mcourt@gmail.com>
2022-01-04 19:12:51 -0500

    Add token requirement for queries

--- app/graphql/queries/fetch_user.rb
+++ app/graphql/queries/fetch_user.rb
@@ -2,8 +2,23 @@ module Queries
   class FetchUser < Queries::BaseQuery
     type Types::UserType, null: false
 
-    def resolve
-      User.first
+    argument :token, String, required: true
+
+    def resolve(token:)
+      User.find_by(id: decrypt(token))
+    end
+
+    private
+
+    def decrypt(token)
+      decrypted = crypt.decrypt_and_verify(token)
+      decrypted.gsub('user-id:', '').to_i
+    rescue ActiveSupport::MessageEncryptor::InvalidMessage
+      raise GraphQL::ExecutionError, 'Invalid token'
+    end
+
+    def crypt
+      ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base[0..31])
     end
   end
 end
