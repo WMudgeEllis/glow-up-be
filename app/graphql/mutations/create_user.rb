c28f1d44e4d844560aa93ba3c8f134d1f3dc21f6
Tanner Dale <tanner@tannerdale.com>
2022-01-27 15:47:07 -0600

    Potential fix

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -6,11 +6,17 @@ module Mutations
 
     def resolve(params:)
       user_params = Hash params
-      begin
-        user = User.create!(user_params)
-        token = user.generate_token
 
+      # user = User.create!(user_params)
+      # token = user.generate_token
+
+      user = User.new(user_params)
+
+      if user.save
         { user: user, token: token }
+      else
+        errors = user.errors.full_messages
+        raise GraphQL::ExecutionError, { errors: errors.to_sentence }
       end
     end
   end
