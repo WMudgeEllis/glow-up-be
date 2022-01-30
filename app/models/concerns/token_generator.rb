972cabfb988675287dcfe1415343f672fdcb54e4
Tanner Dale <tanner@tannerdale.com>
2022-01-30 14:30:57 -0600

    Udpate token encryption depending on environment

--- app/models/concerns/token_generator.rb
+++ app/models/concerns/token_generator.rb
@@ -6,6 +6,10 @@ module TokenGenerator
   private
 
   def crypt
-    ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base[0..31])
+    if Rails.env.production?
+      ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'][0..31])
+    else
+      ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base[0..31])
+    end
   end
 end
